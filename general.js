234c98de274cda268d9255a4d2a2d121e96c233f
vivirenremoto <gafeman@gmail.com>
2023-02-24 10:55:37 +0100

    Create general.js

---
+++ general.js
@@ -0,0 +1,34 @@
+
+$(function(){
+  $('.card_link').click(function(){
+      $('#exampleModalLabel').html( $(this).data('title') );
+      $('#btn_download').attr('href', $(this).attr('href') );
+
+      var devlog_url = $(this).data('devlog');
+      if( devlog_url ){
+          $('#btn_devlog').attr('href', devlog_url).show();
+      }else{
+          $('#btn_devlog').hide();
+      }
+
+      var video_url = $(this).data('video');
+      if( video_url ){
+          $('#btn_video').attr('href', video_url).show();
+      }else{
+          $('#btn_video').hide();
+      }
+
+      $.ajax({
+        method: 'GET',
+        url: '/juegos/' + $(this).data('file') + '.html',
+        dataType: 'html'
+      }).done(function(data) {
+        $('#modal .modal-body').html(data);
+        $('#modal').modal('show'); 
+        setTimeout(function(){
+            $('#modal .modal-body div').scrollTop(0);
+        }, 500);
+      });
+      return false;
+  });
+});
